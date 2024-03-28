a9b27bdb230bdbba7b714b73a78e9dbcbb2054f7
mariabulibasa <maria.bulibasa14@gmail.com>
2024-03-29 00:30:19 +0100

    docker

---
+++ backend/app/schemas/sensor_schemas.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
+
+class SensorDataBaseSchema(BaseModel):
+    timestamp: datetime
+    value: float
+    status: Optional[str] = None
+
+class SensorDataSchema(SensorDataBaseSchema):
+    id: int
+
+class SensorDataCreateSchema(SensorDataBaseSchema):
+    pass
