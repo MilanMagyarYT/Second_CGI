e525e6a2da37e319b69db3482fc422e92e9d9689
mariabulibasa <91207174+mariabulibasa@users.noreply.github.com>
2024-05-16 11:06:47 +0200

    changed files for login

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { PublicClientApplication } from "@azure/msal-browser";
-import { MsalProvider } from "@azure/msal-react";
-import { msalConfig } from "./authConfig";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -18,12 +15,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-const msalInstance = new PublicClientApplication(msalConfig);
-
-ReactDOM.render(
-    <MsalProvider instance={msalInstance}>
-        <App />
-    </MsalProvider>,
-    document.getElementById('root')
-);
